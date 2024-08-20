6fdd0b6f86a87fc36b34d99532f70b5c081d394a
eugnwo <yy7826w@gmail.com>
2024-08-20 12:31:50 +0900

    240820

---
+++ 240820/02.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+	DIR *dirp;
+	int f_cnt = 0;
+	int d_cnt = 0;
+
+	struct dirent *dentry;
+
+	if((dirp = opendir(argv[1])) == NULL) {
+		exit(1);
+	}
+	
+	while ((dentry = readdir(dirp))) {
+		if (dentry -> d_ino != 0) {
+			if ((dirp = opendir(argv[1])) == NULL) {
+				f_cnt++;
+			} 
+			else {
+				d_cnt++;
+			}
+		}
+	}
+	printf("file count = %d\n", f_cnt);
+	printf("directory count = %d\n", d_cnt);
+}
